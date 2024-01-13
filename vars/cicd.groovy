1c6dfbdd2edeeabf75e460346f675ea2dc1da202
NithinDevi <152591852+NithinDevi@users.noreply.github.com>
2024-01-13 11:31:07 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,7 @@ def mavenbuild()
 {
   sh 'mvn package'
 }
-def  gitdeploy(jobname,ip,context)
+def  deploytomcat(jobname,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
     
